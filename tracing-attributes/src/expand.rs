0ce08cb33fddd97d1a2d6531ea07391e487b19ce
Pruthvikar Reddy <2778014+pruthvikar@users.noreply.github.com>
2023-01-17 23:00:32 +0000

    Added Redact format mode that allows logging but truncates the value
    # Conflicts:
    #	tracing-attributes/src/attr.rs
    #	tracing-attributes/src/expand.rs

--- tracing-attributes/src/expand.rs
+++ tracing-attributes/src/expand.rs
@@ -246,6 +246,9 @@ fn gen_block<B: ToTokens>(
                 FormatMode::Debug => Some(quote!(
                     tracing::event!(target: #target, #level_tokens, error = ?e)
                 )),
+                FormatMode::Redact => Some(quote!(
+                    tracing::event!(target: #target, #level_tokens, return = "[redacted]")
+                )),
             }
         }
         _ => None,
@@ -260,6 +263,9 @@ fn gen_block<B: ToTokens>(
                 )),
                 FormatMode::Default | FormatMode::Debug => Some(quote!(
                     tracing::event!(target: #target, #level_tokens, return = ?x)
+                )),
+                FormatMode::Redact => Some(quote!(
+                    tracing::event!(target: #target, #level_tokens, return = "[redacted]")
                 )),
             }
         }
